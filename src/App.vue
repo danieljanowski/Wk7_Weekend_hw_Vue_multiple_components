7cbdada76803836709908449c8faafa031ec57eb
danieljanowski <daniel.janowski@gmail.com>
2020-04-05 12:31:14 +0100

    Mission list choice added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>SpaceX</h1>
+    <mission-list :spaceXData="spaceXData"></mission-list>
+    <!-- <div v-for="(launch, index) in spaceXData">
+          <img :src=launch.links.mission_patch_small alt="mission-logo" height="75">
+          <p>{{index}}: {{launch.mission_name}} - {{Date(launch.launch_date_unix)}} - Success: launch: {{launch.launch_success}} - recovery: {{launch.rocket.fairings}} <a :href=launch.links.video_link>Video</a></p>
+          <p>{{launch.details}}</p>
+          <img :src=launch.links.flickr_images[0] alt="image" height="250">
+          <hr> -->
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MissionList from "@/components/MissionList.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    name: "app",
+    components: {
+      "mission-list": MissionList,
+    },
+
+    data(){
+    return {
+      spaceXData: [],
+    }
+  },
+
+  mounted(){
+    fetch('https://api.spacexdata.com/v3/launches/')
+    .then(apiData => apiData.json())
+    .then(apiDataJson => this.spaceXData = apiDataJson)
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  body{
+    background-color: grey;
+  }
+  h1{text-align: center;}
 </style>
---
+++ src/components/MissionList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <form v-on:submit.prevent>
+    <!-- <input type="text" v-model="search" placeholder="search for country..." v-on:keyup="searchForCountry"> -->
+            <select v-on:change="handleSelect" v-model="selectedMissionFromList">
+                <option disabled value="">Select a mission...</option>
+                <option v-for="mission in spaceXData" :value="mission">{{mission.mission_name}}</option>
+            </select>
+        </form>
+    </div>
+</template>
+
+<script>
+import { eventBus } from '../main.js'
+
+export default {
+    name: "mission-list",
+    props: ["spaceXData"],
+    data(){
+        return{
+            "selectedMissionFromList": {},
+        }
+    },
+    methods: {
+        handleSelect(){
+            eventBus.$emit('selected-mission', this.selectedMissionFromList)
+        }
+
+    },
+};
+
+</script>
+
+<style scoped>
+
+</style>
