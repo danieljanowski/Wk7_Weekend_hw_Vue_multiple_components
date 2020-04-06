5132f0aac95f78197caa30ed74c49215d46ff725
danieljanowski <daniel.janowski@gmail.com>
2020-04-06 08:30:36 +0100

    Homework finished

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
   <div>
-    <h1>SpaceX</h1>
-    <mission-list :spaceXData="spaceXData"></mission-list>
-    <!-- <div v-for="(launch, index) in spaceXData">
-          <img :src=launch.links.mission_patch_small alt="mission-logo" height="75">
-          <p>{{index}}: {{launch.mission_name}} - {{Date(launch.launch_date_unix)}} - Success: launch: {{launch.launch_success}} - recovery: {{launch.rocket.fairings}} <a :href=launch.links.video_link>Video</a></p>
-          <p>{{launch.details}}</p>
-          <img :src=launch.links.flickr_images[0] alt="image" height="250">
-          <hr> -->
-    </div>
+    <section class="flex-container">
+      <header class="flex-item header"><img src="https://www.spacex.com/sites/spacex/files/spacex_logo_white.png" alt="logo"></header>
+      <aside class="flex-item sidebar">
+      </aside>
+      <main class="flex-item main">
+        <h1>SpaceX Mission Database</h1>
+        <mission-list :spaceXData="spaceXData"></mission-list>
+        <div v-if="selectedMission"><mission-detail></mission-detail></div>
+      </main>
+      <aside class="flex-item sidebar">
+      </aside>
+    </section>
   </div>
 </template>
 
 <script>
 import MissionList from "@/components/MissionList.vue";
+import MissionDetail from "@/components/MissionDetail.vue";
 
 export default {
     name: "app",
     components: {
       "mission-list": MissionList,
+      "mission-detail": MissionDetail,
     },
 
     data(){
     return {
       spaceXData: [],
+      selectedMission: {},
     }
   },
 
@@ -36,8 +42,43 @@ export default {
 </script>
 
 <style scoped>
-  body{
-    background-color: grey;
-  }
-  h1{text-align: center;}
+
+.flex-container {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  height: 200px;
+  display: flex;
+  justify-content: space-around;
+  flex-flow: row wrap;
+  align-items: stretch;
+}
+.header {
+  height: 50px;
+  flex: 1 100%;
+  text-align: center;
+  background-color: black;
+}
+.sidebar {
+  flex: 1;
+}
+.main {
+  flex: 20;
+}
+.flex-item {
+  /* background: tomato; */
+  padding: 10px;
+  width: 100px;
+  border: 1px none black;
+  color: black;
+  font-weight: normal;
+  font-size: 1.5em;
+  text-align: left;
+  font-family: 'Hind', sans-serif;
+}
+.sidebar {
+  flex: 1;
+}
+h1 {text-align: center;}
+
 </style>
